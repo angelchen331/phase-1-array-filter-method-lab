cf906460b5f317a7c5e0a376cbd3823d828000e8
Yuchen Chen <angelchen3311@gmail.com>
2022-08-04 17:46:52 -0400

    commit tree d789065b9efd80d929e9d8c81049a7f72526e74b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string) {
+    const result = drivers.filter(driversName => driversName.toLowerCase() === string.toLowerCase())
+    return result;
+}
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(driversName => driversName.startsWith(string))
+    return result;
+}
+
+function matchName(drivers, string) {
+    const result = drivers.filter(driversName => driversName.match)
+    // const result = drivers.filter(driversName => driversName.match() === string.match)
+    // const result = drivers.filter(driversName => driversName === string)
+    return result;
+}
+
